8f0fc2e59584145e519fe95b4a1d0ecb3dfc9861
Adrianne <adriannebradford@gmail.com>
2020-01-27 18:18:48 -0500

    binder setup files

---
+++ install.R
@@ -0,0 +1,22 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
+install.packages('DescTools')
+install.packages('plotrix')
+install.packages('lsr')
+install.packages('pwr')
+install.packages('effsize')
+install.packages('ggpubr')
+install.packages('broom')
+install.packages('rcompanion')
+install.packages('tidyr')
+install.packages("gridExtra")
+install.packages("rsq")
+install.packages("afex")
+install.packages("ggcorrplot") ## make pretty corrplot
+install.packages("GGally") ## scatterplot matrix function
+install.packages("gtrendsR")
+install.packages("olsrr") ## ols diagnostics
+
